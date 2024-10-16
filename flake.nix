4daebd36fb4a157c2160820f9fa4f1e2b3391f53
KrishKittur <kittur.krish@gmail.com>
2024-10-16 02:01:41 -0400

    adding to flake as raw out

--- flake.nix
+++ flake.nix
@@ -8,6 +8,8 @@
     outputs = { self, nixpkgs, ... }:
         let
 
+            pkgs = import nixpkgs { system = "x86_64-linux"; }; # for testing purposes
+
             data_offloading_service_overlay = final: prev: {
                 data_offloading_service = final.callPackage ./default.nix { };
             };
@@ -19,7 +21,35 @@
 
                 overlays.default = nixpkgs.lib.composeManyExtensions my_overlays;
 
-                nixosModules.data-offload-service = import ./module.nix;
+                packages.x86_64-linux.data-offload-service = import ./default.nix { inherit pkgs; }; # for testing purposes
+                packages.x86_64-linux.default = import ./default.nix { inherit pkgs; }; # for testing purposes
+
+                nixosModules.data-offload-service = { config, pkgs, ... }:
+
+                {
+                    options.data_offload_service.enable = pkgs.lib.mkOption {
+                        type = pkgs.lib.types.bool;
+                        default = false;
+                        description = "Enable or disable the data offloading service";
+                    };
+
+                    config = pkgs.lib.mkIf config.data_offload_service.enable {
+
+                        systemd.services.data-offload-service = {
+                            description = "Data Offload Service";
+                            wantedBy = [ "multi-user.target" ];
+                            after = [ "network.target" ];
+
+                            serviceConfig = {
+                                After = [ "network.target" ];
+                                ExecStart = "/usr/bin/python3 ${pkgs.data_offload_service}/bin/offload.py";
+                                Restart = "always";
+                            };
+                        };
+
+                    };
+
+                };
 
             };
 }
