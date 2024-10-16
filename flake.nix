3c579b150d5291cc4b1d01fe6939418cc0f8375a
KrishKittur <kittur.krish@gmail.com>
2024-10-15 23:56:35 -0400

    Added overlay to flake

--- flake.nix
+++ flake.nix
@@ -7,25 +7,24 @@
 
     outputs = { self, nixpkgs, ... }:
         let
-            pkgs = import nixpkgs {
-                system = "x86_64-linux";
+
+            pkgs = import nixpkgs { system = "x86_64-linux"; }; # for testing purposes
+
+            data_offloading_service_overlay = final: prev: {
+                data_offloading_service = final.callPackage ./default.nix { };
             };
+
+            my_overlays = [ data_offloading_service_overlay ];
+
         in
             {
-                packages.x86_64-linux.data-offload-service = import ./default.nix { inherit pkgs; };
-                packages.x86_64-linux.default = import ./default.nix { inherit pkgs; };
+
+                overlays.default = nixpkgs.lib.composeManyExtensions my_overlays;
+
+                packages.x86_64-linux.data-offload-service = import ./default.nix { inherit pkgs; }; # for testing purposes
+                packages.x86_64-linux.default = import ./default.nix { inherit pkgs; }; # for testing purposes
 
                 nixosModules.data-offload-service = import ./module.nix;
 
-                nixosConfigurations.mySystem = nixpkgs.lib.nixosSystem {
-                    system = "x86_64-linux";
-                    modules = [
-                        self.nixosModules.data-offload-service
-                    ];
-
-                    config = {
-                        data-offload-service.enable = true;
-                    };
-                };
             };
 }
