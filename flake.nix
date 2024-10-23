7954c57c8096ea3a5d99d0911e3d36665ca6fac7
Krish Kittur <kittur.krish@gmail.com>
2024-10-23 00:38:43 -0400

    Change xauthority to be correct:

--- flake.nix
+++ flake.nix
@@ -35,7 +35,7 @@
                             after = [ "network.target" ];
                             environment =  {
                                 DISPLAY=":0";
-                                XAUTHORITY="/run/user/1000/.mutter-Xwaylandauth.ZIBAV2";
+                                XAUTHORITY="/run/user/1000/.mutter-Xwaylandauth.W1J4V2";
                             };
                             serviceConfig = {
                                 After = [ "network.target" ];
