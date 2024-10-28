2510d7681dcb5ae46effb3e45d9d47dd8444d5cf
Krish Kittur <kittur.krish@gmail.com>
2024-10-27 21:10:00 -0400

    Make it a user service

--- flake.nix
+++ flake.nix
@@ -29,21 +29,21 @@
 
                     config =  {
 
-                        systemd.services.data-offload-service = {
+                        systemd.user.services.data-offload-service = {
                             description = "Data Offload Service";
                             wantedBy = [ "multi-user.target" ];
                             after = [ "network.target" ];
-                            preStart = ''
-                                AUTH_FILE=$(find /run/user/1000 -name ".mutter-Xwaylandauth.*" | head -n 1)
-                                if [ -z "$AUTH_FILE" ]; then
-                                    echo "WAYLAND_AUTH_PATH not found!" >&2
-                                    exit 1
-                                fi
-                                export XAUTHORITY=$AUTH_FILE
-                            '';
+                            # preStart = ''
+                            #     AUTH_FILE=$(find /run/user/1000 -name ".mutter-Xwaylandauth.*" | head -n 1)
+                            #     if [ -z "$AUTH_FILE" ]; then
+                            #         echo "WAYLAND_AUTH_PATH not found!" >&2
+                            #         exit 1
+                            #     fiz
+                            #     export XAUTHORITY=$AUTH_FILE
+                            # '';
                             environment =  {
                                 DISPLAY=":0";
-                                XAUTHORITY="$XAUTHORITY";
+                                # XAUTHORITY="$XAUTHORITY";
                             };
                             serviceConfig = {
                                 After = [ "network.target" ];
