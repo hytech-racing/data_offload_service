8e2573d5f4fa30b65b5890dec898475bbe98b832
Ben Hall <rcmast3r1@gmail.com>
2024-11-24 16:22:04 -0500

    added requests dep

--- default.nix
+++ default.nix
@@ -5,7 +5,7 @@ pkgs.python311Packages.buildPythonApplication rec {
     pname = "data_offload_service";
     version = "1.0.0";
 
-    buildInputs = [ pkgs.python311Packages.psutil pkgs.python311Packages.tkinter ];
+    buildInputs = [ pkgs.python311Packages.psutil pkgs.python311Packages.tkinter pkgs.python311Packages.requests];
 
     propagatedBuildInputs = buildInputs;
 
